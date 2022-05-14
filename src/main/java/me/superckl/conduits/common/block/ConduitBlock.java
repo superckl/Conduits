62b396b079d99174405cab5960c1ea48240760d2
super <superckl@superckl.me>
2022-05-13 23:47:10 -0500

    Add drops when destroyed (doesn't respect creative mode currently)

--- src/main/java/me/superckl/conduits/common/block/ConduitBlock.java
+++ src/main/java/me/superckl/conduits/common/block/ConduitBlock.java
@@ -7,17 +7,21 @@ import javax.annotation.Nullable;
 import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 import me.superckl.conduits.ModBlocks;
 import me.superckl.conduits.ModItems;
+import me.superckl.conduits.common.item.ConduitItem;
 import me.superckl.conduits.conduit.ConduitType;
 import me.superckl.conduits.conduit.connection.ConduitConnectionMap;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.world.Containers;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.SimpleWaterloggedBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition.Builder;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
@@ -26,6 +30,7 @@ import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ConduitBlock extends Block implements EntityBlock, SimpleWaterloggedBlock{
 
@@ -70,6 +75,22 @@ public class ConduitBlock extends Block implements EntityBlock, SimpleWaterlogge
 	protected void createBlockStateDefinition(final Builder<Block, BlockState> pBuilder) {
 		super.createBlockStateDefinition(pBuilder);
 		pBuilder.add(BlockStateProperties.WATERLOGGED);
+	}
+
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public void onRemove(final BlockState pState, final Level pLevel, final BlockPos pPos, final BlockState pNewState, final boolean pIsMoving) {
+		if(!pState.is(pNewState.getBlock())) {
+			final BlockEntity blockentity = pLevel.getBlockEntity(pPos);
+			if (blockentity instanceof final ConduitBlockEntity conduit)
+				conduit.getConnections().types().forEach(type -> {
+					final RegistryObject<ConduitItem> item = ModItems.CONDUITS.get(type).get(conduit.getTier(type));
+					Containers.dropItemStack(pLevel, pPos.getX(), pPos.getY(), pPos.getZ(), new ItemStack(item::get));
+				});
+
+		}
+		super.onRemove(pState, pLevel, pPos, pNewState, pIsMoving);
 	}
 
 	private static final Map<ConduitConnectionMap, VoxelShape> SHAPE_CACHE = new Object2ObjectOpenHashMap<>(ConduitConnectionMap.states());
